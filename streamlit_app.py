4f7941ea6a20e84624c2cc045adba1b4310eac77
KrisdaKKU <66762533+KrisdaKKU@users.noreply.github.com>
2021-04-22 20:10:36 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,8 @@ public_url = ngrok.connect(port='8501')
 import streamlit as st
 st.title('Primonz')
 p_image_url = st.text_input("ลิ้งค์ภาพ: ")
-st.text(imgurl(p_image_url))
+aw = imgurl(p_image_url)
+st.text(str(aw))
 
 def imgurl(p_image_url: str):
   #file#Image.open(image.file)
