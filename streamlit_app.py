eaf9e7768064e946d7a52a6254a852ae78d07270
KrisdaKKU <66762533+KrisdaKKU@users.noreply.github.com>
2021-04-27 02:07:47 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import requests
 import predict
 import streamlit as st
 
-p_image_ = st.experimental_get_query_params()
+p_image_url = st.experimental_get_query_params()
 def imgurl(p_image_url: str):
   #file#Image.open(image.file)
   r = requests.get(p_image_url, allow_redirects=True)
@@ -14,12 +14,12 @@ def imgurl(p_image_url: str):
 
 
 st.title('Primonz')
-p_image_url = st.text_input("ลิ้งค์ภาพ: ")
+#p_image_url = st.text_input("ลิ้งค์ภาพ: ")
 #if st.button('Excuted'):
-aw = imgurl(p_image_url)
+aw = imgurl(st.text_input("ลิ้งค์ภาพ: "))
 st.image("file2.png")
 st.text(str(aw))
-st.text(p_image_)
+st.text(p_image_url)
 #st.experimental_set_query_params(nameChar="")
 
 #nameChar = str(aw)
